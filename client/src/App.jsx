e0ca20a8621367da02fec7062fce62a5bab69fe3
Sumit Kumar Pandey <officialsumitpandey@gmail.com>
2025-03-13 23:50:35 +0530

    initial route folderstructure and configure tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Layout from "./components/Layout";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Route>
+    </Routes>
+  );
+}
