21b0e7374d8f3761869724db357ad53ca94f5a0b
Sumit Kumar Pandey <officialsumitpandey@gmail.com>
2025-04-13 18:59:46 +0530

    create layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-
+import { Route, Routes } from "react-router-dom";
+import { Layout } from "./components/Layout";
 export default function App() {
   return (
-    <div className="bg-red-800">
-      <Navbar />
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" index element={<h1>Home</h1>} />
+      </Route>
+    </Routes>
   );
 }
